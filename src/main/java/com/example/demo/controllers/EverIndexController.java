adb20c5a8dd53a5d2c77fb8195dffd0b12e7a963
ever <892938772@qq.com>
2019-10-14 15:00:05 +0800

    部署45.76.181.3

--- src/main/java/com/example/demo/controllers/EverIndexController.java
+++ src/main/java/com/example/demo/controllers/EverIndexController.java
@@ -3,6 +3,8 @@ package com.example.demo.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletResponse;
+
 @RestController
 public class EverIndexController {
 
@@ -17,7 +19,12 @@ public class EverIndexController {
     }*/
 
     @RequestMapping(value = "/cyy", method = RequestMethod.GET)
-    public String cyy() {
-        return "cyy";
+    public String cyy(String cyy, HttpServletResponse response) {
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "x-requested-with, Content-Type, token, Accept");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+        return "this is a project created by cyy";
     }
 }
